ed960d1ab4dca29ad08338fcdd5637111b4db6f6
Gabriel Santos <gabrielsmdev@gmail.com>
2022-12-26 23:53:28 -0300

    CTA section

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ const Clients = () =>(
   <section className={`${styles.flexCenter} my-4`}>
     <div className={`${styles.flexCenter} flex-wrap w-full`}>
       {clients.map((client) => (
-        <div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px]`}>
+        <div key={client.id} className={`${styles.flexCenter} flex-1 sm:min-w-[192px] min-w-[120px] sm:mt-0 mt-6`}>
           <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain client-logo cursor-pointer"/>
         </div>
       ))}
